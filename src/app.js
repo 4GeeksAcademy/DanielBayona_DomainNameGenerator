ff49596efbfe75fdcad3831be89e9a597d137940
Daniel Bayona <65182005+Bayunas@users.noreply.github.com>
2024-06-06 16:48:28 +0000

    Finished project Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "a"];
+let adj = ["energetic", "playful", "mighty", "ancient"];
+let noun = ["wizard", "warrior", "thief", "magician"];
+let extensions = ["com", "net", "to", "gov", "us", "io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let y = 0; y < noun.length; y++) {
+      for (let e = 0; e < extensions.length; e++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[y]}.${extensions[e]}`);
+      }
+    }
+  }
+}
